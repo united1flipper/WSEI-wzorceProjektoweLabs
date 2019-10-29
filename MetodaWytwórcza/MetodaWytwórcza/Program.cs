a82b13b042c0f495382d75d9acb8b3eb4c9cc45e
Gajos Bartosz <bartosz.gajos@microsoft.wsei.edu.pl>
2019-10-29 18:17:34 +0100

    FactoryMethod, Prototype

---
+++ MetodaWytwórcza/MetodaWytwórcza/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface IPersonFactory
+    {
+        Person CreatePerson(string name);
+    }
+
+    public class PersonFactory : IPersonFactory
+    {
+        int i = 0;
+        public Person CreatePerson(string name)
+        {
+            var person = new Person();
+            person.Name = name;
+            person.Id = i;
+            i++;
+            return person;
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy!
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            PersonFactory pf = new PersonFactory();
+            var geralt = pf.CreatePerson("Geralt");
+            var ciri = pf.CreatePerson("Ciri");
+            Console.WriteLine(geralt.Name + " " + geralt.Id);
+            Console.WriteLine(ciri.Name + " " + ciri.Id);
+        }
+    }
+
+
+}
---
+++ Prototyp/Prototyp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line()
+        {
+            Start = new Point();
+            End = new Point();
+        }
+
+        public override string ToString()
+        {
+            return $"({Start.X}, {Start.Y}) - ({End.X}, {End.Y})";
+        }
+        public Line DeepCopy()
+        {
+            Line copy = new Line();
+            copy.Start.X = this.Start.X;
+            copy.Start.Y = this.Start.Y;
+            copy.End.X = this.End.X;
+            copy.End.Y = this.End.Y;
+            return copy;
+
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy!
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Line AB = new Line();
+            AB.Start.X = 1;
+            AB.Start.Y = 1;
+            AB.End.X = 8;
+            AB.End.Y = 3;
+            var BC = AB.DeepCopy();
+            BC.Start.X = 19;
+
+            Console.WriteLine(AB);
+            Console.WriteLine(BC);
+        }
+    }
+}
