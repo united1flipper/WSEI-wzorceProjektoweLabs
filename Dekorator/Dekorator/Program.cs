916d4359ca04de4e83e604a5d11b1e96cb669657
Gajos Bartosz <bartosz.gajos@microsoft.wsei.edu.pl>
2019-11-26 17:51:28 +0100

    Dekorator, Most

---
+++ Dekorator/Dekorator/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace WzorceProjektowe
+{
+    // Nie zmieniaj poniższej klasy
+    public interface Vehicle
+    {
+        string Description();
+        double Price();
+    }
+
+    public abstract class Addition : Vehicle
+    {
+        // TODO
+        protected Vehicle Car;
+
+        public Addition(Vehicle v)
+        {
+            Car = v;
+        }
+
+        public abstract string Description();
+
+        public abstract double Price();
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class MazdaMX5 : Vehicle
+    {
+        string description = "Mazda MX5 silnik 2.0";
+        double price = 89000.00;
+
+        public string Description()
+        {
+            return description;
+        }
+        public double Price()
+        {
+            return price;
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class FordFocusMk4 : Vehicle
+    {
+        string description = "Ford Focus silnik EcoBoost 1.5";
+        double price = 69400.00;
+
+        public string Description()
+        {
+            return description;
+        }
+        public double Price()
+        {
+            return price;
+        }
+    }
+
+    public class Premium : Addition
+    {
+        // TODO
+        private string description = ", skórzana tapicerka";
+        private double price = 3000;
+
+        public Premium(Vehicle c) : base(c)
+        {
+        }
+
+        public override string Description()
+        {
+            return Car.Description()+description;
+        }
+
+        public override double Price()
+        {
+            return Car.Price()+price;
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Comfort : Addition
+    {
+        private string description = ", ogrzewana szyba przednia";
+        private double price = 1500;
+        public Comfort(Vehicle c) : base(c)
+        {
+        }
+        public override double Price()
+        {
+            return Car.Price() + price;
+        }
+
+        public override string Description()
+        {
+            return Car.Description() + description;
+        }
+    }
+
+    public class Configure
+    {
+        // TODO
+        public Vehicle AddComfort(Vehicle c)
+        {
+             return c = new Comfort(c);
+        }
+
+        public Vehicle AddPremium(Vehicle c)
+        {
+            return c = new Premium(c);
+        }
+
+        public void PrintSpecification(Vehicle c)
+        {
+            Console.WriteLine($"{c.Description()}; koszt: {c.Price()} zł");
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Configure();
+
+            Vehicle miata = new MazdaMX5();
+            miata = config.AddComfort(miata);
+            miata = config.AddPremium(miata);
+            config.PrintSpecification(miata);
+
+            Vehicle focus = new FordFocusMk4();
+            focus = config.AddComfort(focus);
+            config.PrintSpecification(focus);
+        }
+    }
+}
---
+++ Most/Most/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace WzorceProjektowe
+{
+
+    public abstract class Message
+    {
+        // TODO
+        public abstract void Send();
+    }
+
+
+    public abstract class MessageSystem
+    {
+        // TODO
+        public Message Msg;
+        public void SendMessage()
+        {
+            Msg.Send();
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class AnniversaryMessageSystem : MessageSystem
+    {
+
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class MessageEN : Message
+    {
+        public override void Send()
+        {
+            Console.WriteLine("Happy Birthday!");
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class MessagePL : Message
+    {
+        public override void Send()
+        {
+            Console.WriteLine("Wszystkiego najlepszego!");
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class MessageDE : Message
+    {
+        public override void Send()
+        {
+            Console.WriteLine("Alles Gute!");
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var MsgSystem = new AnniversaryMessageSystem();
+            MsgSystem.Msg = new MessagePL();
+            MsgSystem.SendMessage();
+            MsgSystem.Msg = new MessageEN();
+            MsgSystem.SendMessage();
+            MsgSystem.Msg = new MessageDE();
+            MsgSystem.SendMessage();
+        }
+    }
+}
