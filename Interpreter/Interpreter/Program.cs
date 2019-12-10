00b6a2dd8b2ea8d3329eb3504183084ba8d640cb
Gajos Bartosz <bartosz.gajos@microsoft.wsei.edu.pl>
2019-12-10 18:28:27 +0100

    Iterator, Interpreter

---
+++ Interpreter/Interpreter/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WzorceProjektowe
+{
+    // Nie zmieniaj poniższej klasy
+    class Context
+    {
+        private string _input;
+        private int _output;
+
+        public Context(string input)
+        {
+            this._input = input;
+        }
+
+        public string Input
+        {
+            get { return _input; }
+            set { _input = value; }
+        }
+
+        public int Output
+        {
+            get { return _output; }
+            set { _output = value; }
+        }
+    }
+
+    abstract class Expression
+    {
+        // TODO
+        public abstract int Multiplier();
+        public void Interpret(Context ctx)
+        {
+            int ctxNumber;
+
+            switch (Multiplier())
+            {
+                case 1:
+                    ctxNumber = (int)Char.GetNumericValue(ctx.Input[2]);
+                    break;
+                case 5:
+                    ctxNumber = (int)Char.GetNumericValue(ctx.Input[1]);
+                    break;
+                case 25:
+                    ctxNumber = (int)Char.GetNumericValue(ctx.Input[0]);
+                    break;
+                default:
+                    ctxNumber = 0;
+                    break;
+            }
+            ctx.Output += ctxNumber * Multiplier();
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    class FirstPositionExpression : Expression
+    {
+        public override int Multiplier() { return 1; }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    class SecondPositionExpression : Expression
+    {
+        public override int Multiplier() { return 5; }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    class ThirdPositionExpression : Expression
+    {
+        public override int Multiplier() { return 25; }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    class Program
+    {
+        static void Main()
+        {
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new FirstPositionExpression());
+            tree.Add(new SecondPositionExpression());
+            tree.Add(new ThirdPositionExpression());
+
+            string bathumani = "143";
+            Context context = new Context(bathumani);
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+            Console.WriteLine("{0} = {1}", bathumani, context.Output);
+
+            bathumani = "100";
+            context = new Context(bathumani);
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+            Console.WriteLine("{0} = {1}", bathumani, context.Output);
+        }
+    }
+}
---
+++ Iterator/Iterator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WzorceProjektowe
+{
+    public class Human:IEnumerable<string>
+    {
+        public string Name, FamilyName, Address, Pet;
+        public Human(string name, string familyname, string address, string pet)
+        {
+            Name = name;
+            FamilyName = familyname;
+            Address = address;
+            Pet = pet;
+        }
+
+
+        // TODO
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new Iterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+    public class Iterator : IEnumerator<string>
+    {
+        // TODO
+
+        private Human _human;
+        private int _current = -1;
+
+        public Iterator(Human human)
+        {
+            _human = human;
+        }
+        public string Current
+        {
+            get
+            {
+                switch (_current)
+                {
+                    case 0:
+                        return _human.Name;
+                    case 1:
+                        return _human.FamilyName;
+                    case 2:
+                        return _human.Address;
+                    case 3:
+                        return _human.Pet;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (_current <= 3)
+                _current++;
+            return _current <= 3;
+        }
+
+        public void Reset()
+        {
+            _current = -1;
+        }
+
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Human("Harry", "Potter", "4 Privet Drive, Little Whinging, Surrey", "owl");
+            foreach (var value in person)
+                Console.WriteLine(value);
+            person = new Human("Ronald", "Weasley", "The Burrow outside Ottery St.Catchpole", "rat");
+            foreach (var value in person)
+                Console.WriteLine(value);
+        }
+    }
+}
