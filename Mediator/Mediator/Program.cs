8837e1ff4d536d52f17cf1ca62e9eb9a86644e00
Gajos Bartosz <bartosz.gajos@microsoft.wsei.edu.pl>
2019-12-17 17:35:49 +0100

    Mediator, Łańcuch Zobowiązań

---
+++ Mediator/Mediator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WzorceProjektowe
+{
+    public abstract class AbstractChatMember
+    {
+        // TODO
+        public string Name = null;
+        protected AbstractChatroom _currentChatroom = null;
+        public abstract void Receive(string message);
+        public void Enter(AbstractChatroom chatroom)
+        {
+            if (_currentChatroom != null)
+                Leave();
+            chatroom.Register(this);
+            _currentChatroom = chatroom;
+        }
+
+        public void Leave()
+        {
+            if (_currentChatroom != null)
+            {
+                _currentChatroom.Unregister(this);
+                _currentChatroom = null;            }
+        }
+        public void Send(string msg)
+        {
+            Console.WriteLine($"{Name} --> {msg}");
+            _currentChatroom.Send(msg, this);
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class ChatMember : AbstractChatMember
+    {
+        public ChatMember(string name)
+        {
+            this.Name = name;
+        }
+        public override void Receive(string message)
+        {
+            Console.WriteLine("{0} <-- {1}", Name, message);
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public abstract class AbstractChatroom
+    {
+        protected List<AbstractChatMember> ActiveChatMembers = new List<AbstractChatMember>();
+        public abstract void Send(string message, AbstractChatMember sender);
+        public abstract void Register(AbstractChatMember member);
+        public abstract void Unregister(AbstractChatMember member);
+    }
+    public class Chatroom : AbstractChatroom
+    {
+        // TODO
+        public override void Register(AbstractChatMember member)
+        {
+            Console.WriteLine($"{member.Name} is joining the room");
+            ActiveChatMembers.Add(member);
+        }
+
+        public override void Send(string message, AbstractChatMember sender)
+        {
+            foreach(AbstractChatMember chatMember in ActiveChatMembers)
+            {
+                if (chatMember != sender)
+                    chatMember.Receive(message);
+            }
+        }
+
+        public override void Unregister(AbstractChatMember member)
+        {
+            Console.WriteLine($"{member.Name} has left the room");
+            ActiveChatMembers.Remove(member);
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Tom = new ChatMember("Tom");
+            var Jerry = new ChatMember("Jerry");
+            var chatroom = new Chatroom();
+            Tom.Enter(chatroom);
+            Jerry.Enter(chatroom);
+            Tom.Send("Where is Jerry?");
+            Jerry.Send("Try to guess!");
+            Jerry.Leave();
+        }
+    }
+}
---
+++ Łańcuch Zobowiązań/Łańcuch Zobowiązań/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WzorceProjektowe
+{
+    // Nie zmieniaj poniższego interfejsu
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        void Handle(string request);
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            this._nextHandler = handler;
+            return handler;
+        }
+
+        public virtual void Handle(string request)
+        {
+            if (this._nextHandler != null)
+            {
+                this._nextHandler.Handle(request);
+            }
+            else
+            {
+                Console.WriteLine($"Nikt nie chce tego zjeść: {request}");
+            }
+        }
+    }
+
+    public class MonkeyHandler : AbstractHandler
+    {
+        // TODO
+        public override void Handle(string request)
+        {
+            if (request == "banan")
+                Console.WriteLine($"Małpa zjada {request}");
+            else
+                base.Handle(request);
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class SquirrelHandler : AbstractHandler
+    {
+        public override void Handle(string request)
+        {
+            if (request == "orzech")
+            {
+                Console.WriteLine($"Wiewiórka zjada {request}.");
+            }
+            else
+            {
+                base.Handle(request);
+            }
+        }
+    }
+
+    public class DogHandler : AbstractHandler
+    {
+        // TODO
+        public override void Handle(string request)
+        {
+            if (request == "mięso" || request == "plasterek szynki")
+                Console.WriteLine($"Pies zjada {request}");
+            else
+                base.Handle(request);
+        }
+
+    }
+
+    public class CatHandler:AbstractHandler
+    {
+        // TODO
+        public override void Handle(string request)
+        {
+            if (request == "mięso")
+                Console.WriteLine($"Kot zjada {request}");
+            else
+                base.Handle(request);
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Client
+    {
+        public static void ClientCode(AbstractHandler handler)
+        {
+            foreach (var food in new List<string> { "orzech", "banan", "mięso", "plasterek szynki", "lody" })
+            {
+                Console.WriteLine($"Kto chce {food}?");
+
+                handler.Handle(food);
+            }
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var monkey = new MonkeyHandler();
+            var squirrel = new SquirrelHandler();
+            var dog = new DogHandler();
+            var cat = new CatHandler();
+
+            monkey.SetNext(dog).SetNext(squirrel).SetNext(cat);
+
+            Console.WriteLine("Łańcuch: Małpa > Pies > Wiewiórka > Kot");
+            Client.ClientCode(monkey);
+            Console.WriteLine();
+
+            Console.WriteLine("Podzbiór łańcucha: Wiewiórka > Kot\n");
+            Client.ClientCode(squirrel);
+        }
+    }
+}
