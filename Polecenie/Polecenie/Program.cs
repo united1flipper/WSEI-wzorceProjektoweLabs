ff32009db7b4c4efd8ab6c0a5a9d3b339cf39587
Baartooo <bartek.gajos19@gmail.com>
2020-01-21 17:53:16 +0100

    Polecenie; Stan

---
+++ Polecenie/Polecenie/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace WzorceProjektowe
+{
+    // Nie zmieniaj poniższej implementacji
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    class FirstCommand : ICommand
+    {
+        // TODO
+        public string Text;
+        public FirstCommand(string text)
+        {
+            this.Text = text;
+        }
+        public void Execute()
+        {
+            Console.WriteLine($"Pierwsze polecenie wypisuje: \"{this.Text}\"");
+        }
+    }
+
+    // Nie zmieniaj poniższej implementacji
+    class SecondCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Drugie polecenie nie wypisuje niczego konkretnego");
+        }
+    }
+
+    class ThirdCommand : ICommand
+    {
+        public int Result;
+        // TODO
+        public ThirdCommand(int number)
+        {
+            this.Result = 4 * number;
+        }
+        public void Execute()
+        {
+            Console.WriteLine($"Trzecie polecenie wykonuje obliczenia, które dają {this.Result}");
+        }
+    }
+
+    // Nie zmieniaj poniższej implementacji
+    class Invoker
+    {
+        private ICommand _onStart;
+        private ICommand _onMiddle;
+        private ICommand _onFinish;
+
+        public void SetOnStart(ICommand command)
+        {
+            this._onStart = command;
+        }
+
+        public void SetOnMiddle(ICommand command)
+        {
+            this._onMiddle = command;
+        }
+
+        public void SetOnFinish(ICommand command)
+        {
+            this._onFinish = command;
+        }
+
+        public void ExecuteCommands()
+        {
+            if (this._onStart is ICommand)
+            {
+                this._onStart.Execute();
+            }
+
+            if (this._onMiddle is ICommand)
+            {
+                this._onMiddle.Execute();
+            }
+
+            if (this._onFinish is ICommand)
+            {
+                this._onFinish.Execute();
+            }
+        }
+    }
+
+    // Nie zmieniaj poniższej implementacji
+    class Program
+    {
+        static void Main()
+        {
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new FirstCommand("Jestę programistę"));
+            invoker.SetOnMiddle(new SecondCommand());
+            invoker.SetOnFinish(new ThirdCommand(2));
+
+            invoker.ExecuteCommands();
+        }
+    }
+}
---
+++ Stan/Stan/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace WzorceProjektowe
+{
+    public class CoffeeMachine
+    {
+        public State State { get; set; }
+        public CoffeeMachine()
+        {
+            this.State = new Empty();
+        }
+        public void AddCoffeeGrains()
+        {
+            //TODO
+            this.State.AddCoffeeGrains(this);
+
+        }
+        public void AddMilk()
+        {
+            //TODO
+            this.State.AddMilk(this);
+        }
+        public void MakeBlackCoffee()
+        {
+            //TODO
+            this.State.MakeBlackCoffee(this);
+        }
+        public void MakeWhiteCoffee()
+        {
+            //TODO
+            this.State.MakeWhiteCoffee(this);
+        }
+        public void Process(string command)
+        {
+            Console.WriteLine(command + ": ");
+            switch (command)
+            {
+                case "1":
+                    this.AddCoffeeGrains();
+                    break;
+                case "2":
+                    this.AddMilk();
+                    break;
+                case "3":
+                    this.MakeBlackCoffee();
+                    break;
+                case "4":
+                    this.MakeWhiteCoffee();
+                    break;
+            }
+        }
+    }
+    // Nie zmieniaj poniższej klasy
+    public abstract class State
+    {
+        public abstract void AddCoffeeGrains(CoffeeMachine context);
+        public abstract void AddMilk(CoffeeMachine context);
+        public abstract void MakeBlackCoffee(CoffeeMachine context);
+        public abstract void MakeWhiteCoffee(CoffeeMachine context);
+    }
+    public class Empty : State
+    {
+        //TODO
+        public override void AddCoffeeGrains(CoffeeMachine context)
+        {
+            context.State = new WithCoffeeGrains();
+            Console.WriteLine("Dodano ziarna kawy.");
+        }
+
+        public override void AddMilk(CoffeeMachine context)
+        {
+            context.State = new WithMilk();
+            Console.WriteLine("Dolano mleko.");
+        }
+
+        public override void MakeBlackCoffee(CoffeeMachine context)
+        {
+            Console.WriteLine("Nie można zrobić kawy - brak ziaren.");
+        }
+
+        public override void MakeWhiteCoffee(CoffeeMachine context)
+        {
+            Console.WriteLine("Nie można zrobić kawy - brak ziaren i mleka.");
+        }
+    }
+    public class WithCoffeeGrains : State
+    {
+        //TODO
+        public override void AddCoffeeGrains(CoffeeMachine context)
+        {
+            Console.WriteLine("Pojemnik na ziarna kawy pełny.");
+        }
+
+        public override void AddMilk(CoffeeMachine context)
+        {
+            context.State = new WithCoffeeGrainsAndMilk();
+            Console.WriteLine("Dolano mleko.");
+        }
+
+        public override void MakeBlackCoffee(CoffeeMachine context)
+        {
+            context.State = new Empty();
+            Console.WriteLine("Robię czarną kawę.");
+        }
+
+        public override void MakeWhiteCoffee(CoffeeMachine context)
+        {
+            Console.WriteLine("Nie można zrobić kawy - brak mleka.");
+        }
+    }
+
+    public class WithMilk : State
+    {
+        //TODO
+        public override void AddCoffeeGrains(CoffeeMachine context)
+        {
+            context.State = new WithCoffeeGrainsAndMilk();
+            Console.WriteLine("Dodano ziarna kawy.");
+        }
+
+        public override void AddMilk(CoffeeMachine context)
+        {
+        }
+
+        public override void MakeBlackCoffee(CoffeeMachine context)
+        {            
+        }
+
+        public override void MakeWhiteCoffee(CoffeeMachine context)
+        {
+        }
+    }
+
+    public class WithCoffeeGrainsAndMilk : State
+    {
+        //TODO
+        public override void AddCoffeeGrains(CoffeeMachine context)
+        {
+            Console.WriteLine("Pojemnik na ziarna kawy pełny.");
+        }
+
+        public override void AddMilk(CoffeeMachine context)
+        {
+        }
+
+        public override void MakeBlackCoffee(CoffeeMachine context)
+        {
+            context.State = new WithMilk();
+            Console.WriteLine("Robię czarną kawę.");
+        }
+
+        public override void MakeWhiteCoffee(CoffeeMachine context)
+        {
+            context.State = new Empty();
+            Console.WriteLine("Robię białą kawę.");
+        }
+    }
+
+    // Nie zmieniaj poniższej klasy
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ekspres do kawy");
+            Console.WriteLine("1: Wsyp ziarno");
+            Console.WriteLine("2: Wlej mleko");
+            Console.WriteLine("3: Zrób czarną kawę");
+            Console.WriteLine("4: Zrób białą kawę");
+
+            var machine = new CoffeeMachine();
+
+            machine.Process("3");
+            machine.Process("4");
+            machine.Process("1");
+            machine.Process("4");
+            machine.Process("2");
+            machine.Process("4");
+            machine.Process("1");
+            machine.Process("1");
+            machine.Process("2");
+            machine.Process("3");
+            machine.Process("1");
+            machine.Process("4");
+        }
+    }
+}
